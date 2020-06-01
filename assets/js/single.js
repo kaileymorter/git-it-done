965c3c5154dc4ab7f048a94d781fe1deaa6632f6
Spartacus <k_morter@outlook.com>
2020-06-01 12:43:15 -0600

    added a warning for more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning")
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -35,6 +36,17 @@ var displayIssues = function(issues) {
     }
 };
 
+var displayWarning = function(repo){
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href","https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+}
+
 var getRepoIssues = function(repo){
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -42,6 +54,10 @@ var getRepoIssues = function(repo){
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request");
